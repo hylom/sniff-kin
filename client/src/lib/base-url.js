eaea8c2e1f9c52a18802d68df0e60b936c09df1b
hylom <hylom@hylom.net>
2022-12-12 21:31:08 +0900

    client: support SK_BASE_URL environment variable on development

--- client/src/lib/base-url.js
+++ client/src/lib/base-url.js
@@ -1,3 +1,9 @@
 export function baseUrl() {
+  if (process.env.NODE_ENV === 'production') {
+    return '/';
+  }
+  if (process.env.SK_BASE_URL) {
+    return process.env.SK_BASE_URL;
+  }
   return 'http://localhost:8080/';
 }
