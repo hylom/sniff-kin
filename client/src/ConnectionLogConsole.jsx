79c3ccfeca92fce0498b2ed53f5d54e724072f91
hylom <hylom@hylom.net>
2022-12-13 22:51:15 +0900

    implement live log update

--- client/src/ConnectionLogConsole.jsx
+++ client/src/ConnectionLogConsole.jsx
@@ -8,11 +8,40 @@ export function ConnectionLogConsole() {
   const [logs, setLogs] = useState([]);
   const [error, setError] = useState('');
   const [selectedLog, setSelectedLog] = useState(null);
+  const [polling, setPolling] = useState(false);
+  const pollingInterval = 5000;
 
   useEffect(() => {
-    getLogs().then(r => setLogs(r))
+    getLogs()
+      .then(r => {
+        setLogs(r);
+        setPolling(true);
+      })
       .catch(e => setError(e));
   }, []);
+
+  function updateLogs() {
+    const param = {};
+    const lastId = logs.length ? logs.at(-1)._id : 0;
+    if (lastId) {
+      param.lastId = lastId;
+    }
+    console.log(param);
+    getLogs(param)
+      .then(r => {
+        if (r.length) {
+          setLogs(logs.concat(r));
+        }
+      })
+      .catch(e => setError(e));
+  }
+
+  useEffect(() => {
+    if (polling) {
+      const tId = setInterval(updateLogs, pollingInterval);
+      return () => clearInterval(tId);
+    }
+  }, [updateLogs, polling]);
 
   const onSelectLog = logId => {
     console.log(`${logId} is selected.`);
--- client/src/lib/connection-log.js
+++ client/src/lib/connection-log.js
@@ -1,5 +1,15 @@
 import { get } from './fetch';
 
-export function getLogs(query) {
-  return get('/logs');
+export function getLogs(query={}) {
+  const params = [];
+  if (query.lastId) {
+    params.push(`last_id=${query.lastId}`);
+  }
+
+  let url = '/logs';
+  if (params.length) {
+    url = `${url}?${params.join('&')}`;
+  }
+    
+  return get(url);
 }
